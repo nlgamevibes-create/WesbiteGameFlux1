e565277a58446b31f2992282206c39a87636a55e
nlgamevibes-create <nlgamevibes@gmail.com>
2025-11-05 16:34:19 +0100

    Define Stripe secret key in config.php
    Added Stripe secret key definition for payment processing.

---
+++ config.php
@@ -0,0 +1,3 @@
+<?php
+// Gebruik hier je eigen nieuwe sleutel uit Stripe (nooit publiek tonen!)
+define('STRIPE_SECRET_KEY', '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');
